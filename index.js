256c0d19cbb39b0cc63ebc735306111f04b2b963
AurelienVERRIER <aurelien.verrier@gmail.com>
2022-06-20 16:53:10 +0200

    exercice almost done

---
+++ index.js
@@ -0,0 +1,68 @@
+const integer = 102
+console.log(integer)
+
+const float = 13.9
+console.log(float)
+
+const basic = 34
+console.log(basic)
+
+const stringified = `${basic}`
+console.log(stringified)
+
+const num = 1.5
+const rounded = Math.round(num)
+console.log(rounded)
+
+const test = 12
+const bis = 5
+console.log(test + bis)
+console.log(test - bis)
+console.log(test * bis)
+console.log(test / bis)
+console.log(test % bis)
+
+console.log(999)
+
+console.log(999 % 1)
+console.log(999 % 2)
+console.log(999 % 3)
+console.log(999 % 4)
+console.log(999 % 5)
+console.log("for 999, 1 & 3")
+
+console.log(25)
+
+console.log(25 % 1)
+console.log(25 % 2)
+console.log(25 % 3)
+console.log(25 % 4)
+console.log(25 % 5)
+console.log("for 25, 1 & 5")
+
+console.log(190280)
+
+console.log(190280 % 1)
+console.log(190280 % 2)
+console.log(190280 % 3)
+console.log(190280 % 4)
+console.log(190280 % 5)
+console.log("for 190280, 1, 2, 4 & 5")
+
+console.log(47)
+
+console.log(47 % 1)
+console.log(47 % 2)
+console.log(47 % 3)
+console.log(47 % 4)
+console.log(47 % 5)
+console.log("for 47, only 1")
+
+console.log(691)
+
+console.log(691 % 1)
+console.log(691 % 2)
+console.log(691 % 3)
+console.log(691 % 4)
+console.log(691 % 5)
+console.log("for 691, only 1")
